a18603e8fa354330fe7270781b82ad3016864eef
Jose Nortes <nooors@gmail.com>
2022-09-10 11:05:22 +0200

    fix: enviroments

--- src/app/characters/pages/list/list.component.ts
+++ src/app/characters/pages/list/list.component.ts
@@ -1,20 +1,20 @@
-import { Character } from "./../../../models/characters";
-import { CharactersService } from "./../../../services/characters.service";
-import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
-import { ListDataSource } from "src/app/services/list-data-source";
-import { ActivatedRoute } from "@angular/router";
-import { MatPaginator } from "@angular/material/paginator";
-import { tap } from "rxjs/operators";
+import { Character } from './../../../models/characters';
+import { CharactersService } from './../../../services/characters.service';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ListDataSource } from 'src/app/services/list-data-source';
+import { ActivatedRoute } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-list",
-  templateUrl: "./list.component.html",
-  styleUrls: ["./list.component.scss"],
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit, AfterViewInit {
   character!: Character;
   dataSource!: ListDataSource;
-  displayedColumns = ["name", "race", "gender", "hair", "realm", "spouse"];
+  displayedColumns = ['name', 'race', 'gender', 'hair', 'realm', 'spouse'];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -24,26 +24,22 @@ export class ListComponent implements OnInit, AfterViewInit {
    */
   constructor(
     private httpService: CharactersService,
-    private route: ActivatedRoute,
-    ) {}
+    private route: ActivatedRoute
+  ) {}
 
-    ngOnInit(): void {
-      this.character = this.route.snapshot.data["character"];
-      this.dataSource = new ListDataSource(this.httpService);
-      this.dataSource.loadCharacters();
-    }
+  ngOnInit(): void {
+    this.character = this.route.snapshot.data['character'];
+    this.dataSource = new ListDataSource(this.httpService);
+    this.dataSource.loadCharacters();
+  }
 
-    ngAfterViewInit(): void {
-      this.paginator.page
-      .pipe(
-        tap ( () => this.loadCharactersPage())
-      )
-      .subscribe()
-    }
+  ngAfterViewInit(): void {
+    this.paginator.page.pipe(tap(() => this.loadCharactersPage())).subscribe();
+  }
 
-    loadCharactersPage(){
-      this.dataSource.loadCharacters( this.character.)
-    }
+  loadCharactersPage() {
+    this.dataSource.loadCharacters(this.character);
+  }
   onRowClicked(row: any) {
     console.log(row);
   }
